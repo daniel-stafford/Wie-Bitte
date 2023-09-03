464a90605445c4f103cd42a181d06a3d476c8e2f
daniel-stafford <33048490+daniel-stafford@users.noreply.github.com>
2023-09-03 18:37:37 +0200

    Set up basic app with simple styling and search of wiki

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faSearch } from "@fortawesome/free-solid-svg-icons"
+
+import "./App.css"
+
+function SearchPage() {
+  const [query, setQuery] = useState<string>("")
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(e.target.value)
+  }
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault()
+    if (query) {
+      // Use window.location.href to navigate to an external URL
+      window.location.href = `https://en.wiktionary.org/wiki/${query}#German`
+    }
+  }
+
+  return (
+    <div>
+      <h1 className="animate-title">Wie bitte?</h1>
+      <form onSubmit={handleSubmit} className="search-form">
+        <div className="search-container">
+          <input
+            type="text"
+            placeholder="Schreib ein Wort, Alter!"
+            value={query}
+            onChange={handleInputChange}
+          />
+          <button type="submit" className="button-pulsate">
+            <FontAwesomeIcon icon={faSearch} />
+          </button>
+        </div>
+      </form>
+    </div>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<SearchPage />} />
+        </Routes>
+      </div>
+    </Router>
+  )
 }
 
-export default App;
+export default App
